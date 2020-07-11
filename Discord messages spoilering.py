8dd09486c3d481e4bf1e17326710e1548368d89f
Arkya29 <61916582+Arkya29@users.noreply.github.com>
2020-07-11 23:36:20 +0200

    Update Discord messages spoilering.py

--- Discord messages spoilering.py
+++ Discord messages spoilering.py
@@ -2,10 +2,10 @@
 print("/!\ VERSION BETA : le programme ne prend pas en charge les sauts de ligne /!\ ")
 message = input("Dans quel message voulez-vous ajouter des spoilers entre chaque lettre ?")  # On demande le message à traiter.
 message = str(message)  # Pour qu'il n'y ai pas de problème avec les chiffres, on assure la conversion en texte.
-nombreDeCaractere = len(message)  # On regarde combien il y a de caratères
+nombreDeCaractere = len(message)  # On regarde combien il y a de caratères.
 caractere = 0
 print()  # Saut de ligne avant le résultat.
-while nombreDeCaractere > caractere:  # Tant qu'on a pas traité chaque caractère.
+while nombreDeCaractere > caractere:  # Tant qu'on a pas traité chaque caractère du message.
     print("||" + message[caractere] + "||", end="")  # On poste le message avec les spoilers, sans saut de ligne entre chaque.
     caractere = caractere + 1
 input("\n\nAppuyez sur la touche \"Entrer\" pour quitter le programme.")  # Quitter le programme.
